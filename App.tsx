8c3b6efa5af7aabd78f60d83e6431a3174c8b724
Ethan Cheatham <ethanwcheatham@gmail.com>
2020-03-02 17:04:43 -0800

    bluetooth recording

--- App.tsx
+++ App.tsx
@@ -1,30 +1,39 @@
-import React from 'react';
-import {View, Button} from 'react-native';
-import {BleManager, Device} from 'react-native-ble-plx';
+import React, {useState} from 'react';
+import {View, Button, Text} from 'react-native';
+import {BleManager} from 'react-native-ble-plx';
 
 declare var global: {HermesInternal: null | {}};
 const manager: BleManager = new BleManager();
 
+let scannedDevices = new Set();
+
 const App = () => {
-  const printDevice = (device: any) => {
-    if (device.name) {
-      console.log(device.name);
+
+  const [devices, setDevices] = useState(new Set());
+
+  const handleDevice = (newDevice: any) => {
+    scannedDevices.add(newDevice.id);
+    if (newDevice.name) {
+      console.log(newDevice.id, newDevice.name)
     }
   };
 
   const scan = () => {
     manager.startDeviceScan(null, null, (error, scannedDevice) =>
-      printDevice(scannedDevice),
+      handleDevice(scannedDevice),
     );
   };
 
   const stop = () => {
     manager.stopDeviceScan();
+    let updatedDevices = new Set([...devices, ...scannedDevices]);
+    setDevices(updatedDevices);
   };
 
   return (
     <View style={{flex: 1, justifyContent: 'space-between'}}>
       <Button title="Scan" onPress={() => scan()} />
+      <Text style={{alignSelf: 'center'}}>{devices.size}</Text>
       <Button title="Stop" onPress={() => stop()} />
     </View>
   );
